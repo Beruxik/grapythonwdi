51e95d9872a4f160144624ebd25df3e42d3f276f
Beruxik <57355176+Beruxik@users.noreply.github.com>
2020-10-23 09:51:52 +0200

    Add files via upload

--- gra.py
+++ gra.py
@@ -1,18 +1,22 @@
 import random
-x = random.randint(0, 100)
+x = random.randint(1, 100)
 print(x)
 usernum = 0
+gorna = 100
+dolna = 1
 
 while usernum != x:
     try:
-        usernum = int(input('Podaj liczbę od 1 do 100: '))
-        if usernum > x:
+        usernum = int(input('Podaj liczbę od ' + str(dolna) + ' do ' + str(gorna) + ': '))
+        if x < usernum <= gorna:
             print('za duża liczba')
-            usernum = int(input('Podaj liczbę od 1 do 100: '))
-        if usernum < x:
+            gorna = usernum - 1
+        elif x > usernum >= dolna:
             print('za mała liczba')
-            usernum = int(input('Podaj liczbę od 1 do 100: '))
-        if usernum == x:
+            dolna = usernum + 1
+        elif usernum > gorna or usernum < dolna:
+            print('Podaj liczbę z podanego zakresu!')
+        else:
             print('brawo, mój przyjacielu')
     except ValueError:
         print('Nie podałeś liczby!')
